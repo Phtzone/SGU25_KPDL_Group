9e62fd6b83db2e94dfd386cc8f171f644037be7d
Phtzone <pht.github@gmail.com>
2025-10-12 16:28:05 +0700

    add

--- Lab03/winequality-red/_print_nb.py
+++
@@ -1,7 +0,0 @@
-import json
-from pathlib import Path
-nb = json.loads(Path("winequality-red.ipynb").read_text(encoding="utf-8"))
-for i, cell in enumerate(nb["cells"]):
-    if cell.get("cell_type") == "code":
-        print(f"\n# Cell {i}\n")
-        print(''.join(cell.get("source", [])))
